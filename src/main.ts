2323fbf54a2bff4c8d4631e900e1fe89c4a2b83a
KaungZayY <kaungzayyan19@gmail.com>
2026-07-13 16:54:54 +0700

    global validation pipe & api versioning -> now all routes prefix with v1 by default

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
   app.useLogger(app.get(Logger));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    })
+  );
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
