30c6ee469d468df8046cfbc287617c3443d4ef7d
KaungZayY <kaungzayyan19@gmail.com>
2026-07-20 19:52:07 +0700

    users tbl fields set nullable

--- prisma/migrations/20260720125126_create_tables_user_organization_member_project/migration.sql
+++ prisma/migrations/20260720125126_create_tables_user_organization_member_project/migration.sql
@@ -10,9 +10,9 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password_hash" TEXT NOT NULL,
-    "verified_at" TIMESTAMP(3) NOT NULL,
-    "last_login_at" TIMESTAMP(3) NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL,
+    "verified_at" TIMESTAMP(3),
+    "last_login_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
